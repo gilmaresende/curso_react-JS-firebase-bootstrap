666b145f5a93aab00a0e17db88a90db2ab69915d
Gilmar F <gilmar.lara@touchcomp.com.br>
2020-12-04 13:07:34 -0300

    Modelo aquivo config Firebase

---
+++ evento/src/config/firebase2.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
