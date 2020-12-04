95eff42e6a8d650de859525a4918ea44241f4a51
Gilmar F <gilmar74796@gmail.com>
2020-12-04 00:10:09 -0300

    8. Deixando as mensagens da tela de login dinâmica

--- evento/src/view/login/index.js
+++ evento/src/view/login/index.js
@@ -1,7 +1,27 @@
-import React from "react";
 import "./login.css";
 
+import React, { useState } from "react";
+import Firebase from "../../config/firebase";
+import "firebase/auth";
+
 function Login() {
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+  const [msgTipo, setMsgTipo] = useState();
+
+  function logar() {
+    Firebase.auth()
+      .signInWithEmailAndPassword(email, senha)
+      .then((response) => {
+        {
+          setMsgTipo("SUCESSO");
+        }
+      })
+      .catch((error) => {
+        setMsgTipo("ERRO");
+      });
+  }
+
   return (
     <div className="login-content d-flex aling-center">
       {" "}
@@ -20,6 +40,7 @@ function Login() {
         </div>
 
         <input
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           id="inputEmail"
           className="form-control my-2"
@@ -27,21 +48,33 @@ function Login() {
         />
 
         <input
+          onChange={(e) => setSenha(e.target.value)}
           type="password"
           id="inputPassword"
           className="form-control my-2"
           placeholder="Senha"
         />
 
-        <button className="btn btn-lg btn-block btn-login" type="submit">
-          Sign in
+        <button
+          onClick={logar}
+          className="btn btn-lg btn-block btn-login"
+          type="button"
+        >
+          Logar
         </button>
 
         <div className="msg-login text-white text-center my-5">
-          <span>
-            <strong>WoW!</strong>Você esta conectado! &#128526;
-            <strong>Ops!</strong>Verificaque estão corretos! &#128546;
-          </span>
+          {msgTipo === "SUCESSO" ? (
+            <span>
+              <strong>WoW!</strong> Você esta conectado! &#128526;
+            </span>
+          ) : msgTipo === "ERRO" ? (
+            <span>
+              <strong>Ops!</strong> Verificaque estão corretos! &#128546;
+            </span>
+          ) : (
+            ""
+          )}
         </div>
         <div className="opcoes-login mt-5 text-center">
           <a href="#" className="mx-2">
